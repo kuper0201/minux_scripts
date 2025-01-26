d75f8f905060f59f82dca194fe21dfa7ca743245
kuper0201 <kuper171@gmail.com>
2025-01-26 07:53:18 +0000

    first init

---
+++ install_yay.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# 임시 사용자 계정 정보
+USERNAME="tempuser"
+
+echo "==> 임시 사용자 계정 생성 중..."
+useradd -m -G wheel -s /bin/bash "$USERNAME"
+echo "$USERNAME:password" | chpasswd
+
+# sudo 권한 부여
+echo "==> 임시 사용자에 sudo 권한 부여..."
+echo "%wheel ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
+
+# 시스템 업데이트 및 기본 패키지 설치 (루트 권한)
+pacman -Syu --noconfirm
+pacman -S --noconfirm base-devel git
+
+# 임시 사용자로 전환하여 yay 설치
+su - "$USERNAME" -c "
+    git clone https://aur.archlinux.org/yay.git &&
+    cd yay &&
+    makepkg -si --noconfirm &&
+    cd .. &&
+    rm -rf yay
+"
+
+# 임시 사용자 제거
+userdel -r "$USERNAME"
+
+# sudoers 파일 복원
+sed -i '/%wheel ALL=(ALL) NOPASSWD: ALL/d' /etc/sudoers
