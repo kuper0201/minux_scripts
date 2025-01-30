6cff7ba942254172c96faa708462d8a279f0b0eb
kuper0201 <kuper171@gmail.com>
2025-01-30 22:59:29 +0900

    Update install_packages.sh

--- install_packages.sh
+++ install_packages.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
-pacman -Syu --noconfirm linux-zen-headers networkmanager pulseaudio touchegg
+pacman -Syu --noconfirm linux-zen-headers networkmanager pulseaudio touchegg tlp
 systemctl enable touchegg
+systemctl enable tlp
+
+# Tap to Click
+echo -e "Section \"InputClass\"
+    Identifier \"libinput touchpad catchall\"
+    MatchIsTouchpad \"on\"
+    Option \"Tapping\" \"on\"
+    Option \"TappingDragLock\" \"on\"
+EndSection" > /etc/X11/xorg.conf.d/40-libinput.conf
 
 # check install_yay.sh
 # yay -Syu --noconfirm broadcom-wl-dkms
