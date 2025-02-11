55375cf3a50a838c11979d4b3651d0d01a9ef35d
kuper0201 <kuper171@gmail.com>
2025-02-11 17:50:09 +0900

    Update install_packages.sh

--- install_packages.sh
+++ install_packages.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-pacman -Syu --noconfirm linux-lts-headers broadcom-wl-dkms networkmanager pulseaudio touchegg tlp nano
+pacman -Syu --needed --noconfirm linux-lts-headers broadcom-wl-dkms networkmanager git base-devel pulseaudio touchegg tlp nano
+pacman -Syu --needed --noconfirm fcitx5 fcitx5-im fcitx5-hangul fcitx5-configtool fcitx5-qt fcitx5-gtk
+pacman -Syu --needed --noconfirm jq network-manager-applet i3-gaps xorg-server xorg-xinit pcmanfm-gtk3 lxappearance alacritty xdotool kvantum kvantum-theme-materia materia-gtk-theme kvantum-qt5 qt5ct qt6ct libnotify
+pacman -Syu --needed --noconfirm lightdm lightdm-gtk-greeter
+
+systemctl enable lightdm
 systemctl enable touchegg
 systemctl enable tlp
 
