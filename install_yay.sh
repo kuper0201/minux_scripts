069b8bd3fa65187948cea40c7aaa970d5473c47a
kuper0201 <kuper171@gmail.com>
2025-02-11 17:52:13 +0900

    Update install_yay.sh

--- install_yay.sh
+++ install_yay.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
 
 # 임시 사용자 계정 정보
-USERNAME="tempuser"
+USERNAME="user3h3kd09e"
 
 useradd -m -G wheel -s /bin/bash "$USERNAME"
 echo "$USERNAME:password" | chpasswd
 
 # sudo 권한 부여
 echo "%wheel ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
-
-# 시스템 업데이트 및 기본 패키지 설치 (루트 권한)
-pacman -Syu --noconfirm base-devel git
 
 # 임시 사용자로 전환하여 yay 설치
 su - "$USERNAME" -c "
@@ -19,7 +16,7 @@ su - "$USERNAME" -c "
     makepkg -si --noconfirm &&
     cd .. &&
     rm -rf yay &&
-    yay -Syu --noconfirm downgrade
+    yay -Syu --noconfirm downgrade polybar rofi dunst compton feh ttf-nanum
 "
 
 # 임시 사용자 제거
